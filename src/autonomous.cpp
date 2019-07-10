f19353004f7cf9678aafeb422dda9635fa569839
Chronomly <chronomly@gmail.com>
2019-07-10 10:28:26 -0400

    Add auton functions

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,16 +1,60 @@
 #include "main.h"
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-void autonomous() {
+// Distance in inches, Speed in rpms, Wait in milliseconds
+void drive(double distance, int speed, int wait){
+
+    double wheel_radius = 2; //inches
+    double const pi = 3.14159265;
+    //1 rotation will move the robot 2*pi*wheel_radius inches
+    double rotations = distance/(pi*2*wheel_radius);
+    left_mtr.move_relative(rotations, speed);
+    r_left_mtr.move_relative(rotations, speed);
+    right_mtr.move_relative(rotations, speed);
+    r_right_mtr.move_relative(rotations,speed);
+
+    rotations += left_mtr.get_position();
+
+    while (!((left_mtr.get_position() < rotations + 0.5) && (left_mtr.get_position() > rotations - 0.5 ))) {
+        pros::delay(5);
+	}
+
+    pros::delay(wait);
+
+}
+
+// Degrees of turn.  Speed is RPM, Wait is milliseconds
+void turn(double degrees, int speed, int wait){
+
+    double one_rotation_turn_degrees = 115; //customize to your robot
+    double rotations = degrees/one_rotation_turn_degrees;
+    left_mtr.move_relative(rotations, speed);
+    r_left_mtr.move_relative(rotations, speed);
+    right_mtr.move_relative(-rotations, speed);
+    r_right_mtr.move_relative(-rotations,speed);
+
+    rotations += left_mtr.get_position();
+
+    while (!((left_mtr.get_position() < rotations + 0.5) && (left_mtr.get_position() > rotations -0.5 ))) {
+        pros::delay(20);
+    }
     
+    pros::delay(wait);
+
+}
+
+
+
+void autonomous() {
+
+  //front_left_mtr.move_relative(10, 100);
+  drive(60,100,1000);
+  drive(-24,100,1000);
+  drive(24,100,1000);
+  drive(-60,100,1000);
+  //turn(360,100,1000);
+  //turn(-180,100,1000);
+  //turn(100,100,1000);
+  turn(-360,100,1000);
+  turn(360, 100, 1000);
+
 }
--- src/motors.cpp
+++ src/motors.cpp
@@ -4,7 +4,7 @@
 pros::Motor left_mtr(9,MOTOR_GEARSET_18,false,MOTOR_ENCODER_ROTATIONS);
 pros::Motor right_mtr(10,MOTOR_GEARSET_18,true,MOTOR_ENCODER_ROTATIONS);
 pros::Motor r_left_mtr(19,MOTOR_GEARSET_18,false,MOTOR_ENCODER_ROTATIONS);
-pros::Motor r_right_mtr(20,MOTOR_GEARSET_18,true,MOTOR_ENCODER_ROTATIONS);
+pros::Motor r_right_mtr(18,MOTOR_GEARSET_18,true,MOTOR_ENCODER_ROTATIONS);
 
 // Arm & Claw Motors
 pros::Motor claw_mtr(7,MOTOR_GEARSET_36,false,MOTOR_ENCODER_ROTATIONS);
