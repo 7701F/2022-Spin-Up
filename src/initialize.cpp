4caffc4df47b68fd663c954db1c13d0fd4d48f52
Michael Gummere <me@disilla.org>
2019-10-08 08:19:24 -0400

    fix: unignore .vscode and reignore bin/

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -1,14 +1,14 @@
 #include "main.h"
 
-// void on_center_button() {
-// 	static bool pressed = false;
-// 	pressed = !pressed;
-// 	if (pressed) {
-// 		pros::lcd::set_text(2, "I was pressed!");
-// 	} else {
-// 		pros::lcd::clear_line(2);
-// 	}
-// }
+void on_center_button() {
+	static bool pressed = false;
+	pressed = !pressed;
+	if (pressed) {
+		pros::lcd::set_text(2, "I was pressed!");
+	} else {
+		pros::lcd::clear_line(2);
+	}
+}
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
