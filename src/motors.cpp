fe1650d02c614f145df501ca2a1dc8640e07d98e
Michael Gummere <me@disilla.org>
2020-01-24 09:45:02 -0500

    feat: finish things

--- src/motors.cpp
+++ src/motors.cpp
@@ -1,8 +1,12 @@
 #include "main.h"
 
 // Driving Motors
-pros::Motor left_mtr(9,MOTOR_GEARSET_18,false,MOTOR_ENCODER_ROTATIONS);
-pros::Motor right_mtr(10,MOTOR_GEARSET_18,true,MOTOR_ENCODER_ROTATIONS);    
-// Arm & Claw Motors
-pros::Motor claw_mtr(7,MOTOR_GEARSET_36,false,MOTOR_ENCODER_ROTATIONS);
-pros::Motor arm_mtr(8,MOTOR_GEARSET_36,false,MOTOR_ENCODER_ROTATIONS);
+pros::Motor left_mtr(12,MOTOR_GEARSET_18,false,MOTOR_ENCODER_ROTATIONS);
+pros::Motor right_mtr(11,MOTOR_GEARSET_18,true,MOTOR_ENCODER_ROTATIONS);    
+// Arm Motors
+pros::Motor arm0_mtr(18,MOTOR_GEARSET_36,false,MOTOR_ENCODER_ROTATIONS);
+pros::Motor arm1_mtr(19,MOTOR_GEARSET_36,false,MOTOR_ENCODER_ROTATIONS);
+pros::Motor arm2_mtr(20,MOTOR_GEARSET_36,false,MOTOR_ENCODER_ROTATIONS);
+
+// Claw Motor
+pros::Motor claw_mtr(14,MOTOR_GEARSET_36,false,MOTOR_ENCODER_ROTATIONS);
