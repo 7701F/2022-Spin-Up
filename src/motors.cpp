7fcc1ba17b87c1abb0a91763a3e155e8aecfde17
Grey <chronomly@gmail.com>
2020-02-10 12:05:44 -0500

    intake now on right stick

--- src/motors.cpp
+++ src/motors.cpp
@@ -3,11 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
+
 #include "main.h"
 
 // Driving Motors
-pros::Motor left_mtr(12,MOTOR_GEARSET_18,true,MOTOR_ENCODER_ROTATIONS);
-pros::Motor right_mtr(11,MOTOR_GEARSET_18,false,MOTOR_ENCODER_ROTATIONS);
+pros::Motor left_mtr(12,MOTOR_GEARSET_18,false,MOTOR_ENCODER_ROTATIONS);
+pros::Motor right_mtr(11,MOTOR_GEARSET_18,true,MOTOR_ENCODER_ROTATIONS);
 
 // Intake Motors
 pros::Motor intake_mtr(20,MOTOR_GEARSET_36,false,MOTOR_ENCODER_ROTATIONS);
