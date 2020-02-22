5a028aa30080735d46f60a6af044ed2e7cff85f4
Grey <chronomly@gmail.com>
2020-02-22 07:22:02 -0500

    aaaaaaaarrrrrrrrrrrrrrmmmmmmmmmmmms

--- include/motors.h
+++ include/motors.h
@@ -7,5 +7,7 @@ extern pros::Motor right_mtr;
 extern pros::Motor angler_mtr;
 extern pros::Motor intake_mtr;
 extern pros::Motor intake1_mtr;
+extern pros::Motor intakearm_mtr;
+extern pros::Motor intakearm1_mtr;
 
 #endif
