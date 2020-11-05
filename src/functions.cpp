b954d2b2a1b912ecab671fe9da3f9f102c8861c1
Michael Gummere <me@disilla.org>
2020-11-05 17:33:25 -0500

    update pros and add clion stuff

--- src/functions.cpp
+++ src/functions.cpp
@@ -9,40 +9,40 @@
 // Distance in inches, Speed in rpms, Wait in milliseconds
 void drive(double distance, int speed, int wait) {
 
-  double wheel_radius = 2.065; // inches
-  double const pi =
-	  3.14159265; // 1 rotation will move the robot 2*pi*wheel_radius inches
-  double rotations = distance / (pi * 2 * wheel_radius);
+    double wheel_radius = 2.065; // inches
+    double const pi =
+            3.14159265; // 1 rotation will move the robot 2*pi*wheel_radius inches
+    double rotations = distance / (pi * 2 * wheel_radius);
 
-  leftMtr.move_relative(rotations, speed);
-  rightMtr.move_relative(rotations, speed);
+    leftMtr.move_relative(rotations, speed);
+    rightMtr.move_relative(rotations, speed);
 
-  rotations += leftMtr.get_position();
+    rotations += leftMtr.get_position();
 
-  while (!((leftMtr.get_position() < rotations + 0.5) &&
-		   (leftMtr.get_position() > rotations - 0.5))) {
-	pros::delay(5);
-  }
+    while (!((leftMtr.get_position() < rotations + 0.5) &&
+             (leftMtr.get_position() > rotations - 0.5))) {
+        pros::delay(5);
+    }
 
-  pros::delay(wait);
+    pros::delay(wait);
 }
 
 // Degrees of turn.  Speed is RPM, Wait is milliseconds
 void turn(double degrees, int speed, int wait) {
 
-  double one_rotation_turn_degrees = 115; // customize to your robot
-  double rotations = degrees / one_rotation_turn_degrees;
-  leftMtr.move_relative(rotations, speed);
-  rightMtr.move_relative(-rotations, speed);
+    double one_rotation_turn_degrees = 115; // customize to your robot
+    double rotations = degrees / one_rotation_turn_degrees;
+    leftMtr.move_relative(rotations, speed);
+    rightMtr.move_relative(-rotations, speed);
 
-  rotations += leftMtr.get_position();
+    rotations += leftMtr.get_position();
 
-  while (!((leftMtr.get_position() < rotations + 0.5) &&
-		   (leftMtr.get_position() > rotations - 0.5))) {
-	pros::delay(20);
-  }
+    while (!((leftMtr.get_position() < rotations + 0.5) &&
+             (leftMtr.get_position() > rotations - 0.5))) {
+        pros::delay(20);
+    }
 
-  pros::delay(wait);
+    pros::delay(wait);
 }
 
 // angle the tray
