7b26dcf727740919ed55ab359c864256321a38b2
Chronomly <me@disilla.org>
2021-03-02 15:23:14 -0500

    recent changes

--- src/display.cpp
+++ src/display.cpp
@@ -14,17 +14,18 @@ void display()
 {
 	static lv_style_t background_style;
 	lv_style_copy(&background_style, &lv_style_plain);
-	background_style.body.main_color = LV_COLOR_WHITE;
-	background_style.body.grad_color = LV_COLOR_WHITE;
+	background_style.body.main_color = LV_COLOR_BLACK;
+	background_style.body.grad_color = LV_COLOR_BLACK;
 
 	lv_obj_t *scr = lv_obj_create(NULL, NULL);
 	lv_obj_set_style(scr, &background_style);
 	lv_scr_load(scr);
 
 	lv_obj_t *obj = lv_obj_create(lv_scr_act(), NULL);
-	lv_obj_set_size(obj, 400, 209);
+	// lv_obj_set_size(obj, 282, 209);
+	lv_obj_set_size(obj, 50, 40);
 	lv_obj_set_style(obj, &lv_style_transp);
-	lv_obj_align(obj, NULL, LV_ALIGN_CENTER, 0, -20);
+	lv_obj_align(obj, NULL, LV_ALIGN_CENTER, 0, 0);
 
 	// Gif *gif = new Gif("/usd/image.gif", obj);
 
@@ -32,27 +33,27 @@ void display()
 	static lv_style_t title_style;
 	lv_style_copy(&title_style, &lv_style_plain);
 	title_style.text.font = &lv_font_dejavu_20;
-	title_style.text.color = LV_COLOR_BLACK;
+	title_style.text.color = LV_COLOR_WHITE;
 
 	// text
 	lv_obj_t *display_title = lv_label_create(scr, NULL);
 	lv_obj_set_style(display_title, &title_style);
 	lv_label_set_text(display_title, "Hard disk failure is imminent");
-	lv_obj_align(display_title, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, -15);
+	lv_obj_align(display_title, NULL, LV_ALIGN_IN_TOP_LEFT, 0, 0);
 
-	/*Describe the color for the needles*/
-	static lv_color_t needle_colors[1];
-	needle_colors[0] = LV_COLOR_RED;
+	// /*Describe the color for the needles*/
+	// static lv_color_t needle_colors[1];
+	// needle_colors[0] = LV_COLOR_RED;
 
-	/*Create a gauge*/
-	lv_obj_t *gauge1 = lv_gauge_create(lv_scr_act(), NULL);
-	lv_gauge_set_needle_count(gauge1, 1, needle_colors);
-	lv_obj_set_size(gauge1, 200, 200);
-	lv_obj_align(gauge1, NULL, LV_ALIGN_CENTER, 0, 0);
-	lv_gauge_set_range(gauge1, 0, 10);
-	lv_gauge_set_critical_value(gauge1, 4.5);
+	// /*Create a gauge*/
+	// lv_obj_t *gauge1 = lv_gauge_create(lv_scr_act(), NULL);
+	// lv_gauge_set_needle_count(gauge1, 1, needle_colors);
+	// lv_obj_set_size(gauge1, 200, 200);
+	// lv_obj_align(gauge1, NULL, LV_ALIGN_CENTER, 0, 0);
+	// lv_gauge_set_range(gauge1, 0, 10);
+	// lv_gauge_set_critical_value(gauge1, 4.5);
 
-	lv_gauge_set_value(gauge1, 0, 15);
+	// lv_gauge_set_value(gauge1, 0, 15);
 
 	// while (true) {
 	// 	lv_label_set_text(display_title, "Connection Pending");
