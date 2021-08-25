be38398d5497f8d05c57eedb6066dffabe98bf6b
Michael Gummere <me@disilla.org>
2021-08-25 11:02:05 -0400

    Comment outdated functions

--- src/functions.cpp
+++ src/functions.cpp
@@ -6,87 +6,87 @@
 
 #include "main.h"
 
-// Distance in inches, Speed in rpms, Wait in milliseconds
-void drive(double distance, int speed, int wait)
-{
-	double wheel_radius = 2.065; // inches
-	double const pi =
-		3.141592653589793238462643383279502884197169399375105820974944592307816406286; // 1 rotation will move the robot 2*pi*wheel_radius inches
-	double rotations = distance / (pi * 2 * wheel_radius);
+// // Distance in inches, Speed in rpms, Wait in milliseconds
+// void drive(double distance, int speed, int wait)
+// {
+// 	double wheel_radius = 2.065; // inches
+// 	double const pi =
+// 		3.141592653589793238462643383279502884197169399375105820974944592307816406286; // 1 rotation will move the robot 2*pi*wheel_radius inches
+// 	double rotations = distance / (pi * 2 * wheel_radius);
 
-	leftMtr.move_relative(rotations, speed);
-	rightMtr.move_relative(rotations, speed);
+// 	leftMtr.move_relative(rotations, speed);
+// 	rightMtr.move_relative(rotations, speed);
 
-	rotations += leftMtr.get_position();
+// 	rotations += leftMtr.get_position();
 
-	while (!((leftMtr.get_position() < rotations + 0.5) &&
-			 (leftMtr.get_position() > rotations - 0.5)))
-	{
-		pros::delay(5);
-	}
+// 	while (!((leftMtr.get_position() < rotations + 0.5) &&
+// 			 (leftMtr.get_position() > rotations - 0.5)))
+// 	{
+// 		pros::delay(5);
+// 	}
 
-	pros::delay(wait);
-}
+// 	pros::delay(wait);
+// }
 
-// Degrees of turn.  Speed is RPM, Wait is milliseconds
-void turn(double degrees, int speed, int wait)
-{
-	double one_rotation_turn_degrees = 115; // customize to your robot
-	double rotations = degrees / one_rotation_turn_degrees;
-	leftMtr.move_relative(rotations, speed);
-	rightMtr.move_relative(-rotations, speed);
+// // Degrees of turn.  Speed is RPM, Wait is milliseconds
+// void turn(double degrees, int speed, int wait)
+// {
+// 	double one_rotation_turn_degrees = 115; // customize to your robot
+// 	double rotations = degrees / one_rotation_turn_degrees;
+// 	leftMtr.move_relative(rotations, speed);
+// 	rightMtr.move_relative(-rotations, speed);
 
-	rotations += leftMtr.get_position();
+// 	rotations += leftMtr.get_position();
 
-	while (!((leftMtr.get_position() < rotations + 0.5) &&
-			 (leftMtr.get_position() > rotations - 0.5)))
-	{
-		pros::delay(20);
-	}
+// 	while (!((leftMtr.get_position() < rotations + 0.5) &&
+// 			 (leftMtr.get_position() > rotations - 0.5)))
+// 	{
+// 		pros::delay(20);
+// 	}
 
-	pros::delay(wait);
-}
+// 	pros::delay(wait);
+// }
 
-// eject spheres
-void eject(double degrees, int speed, int wait)
-{
+// // eject spheres
+// void eject(double degrees, int speed, int wait)
+// {
 
-	double one_rotation_turn_degrees = 115; // customize to your robot
-	double rotations = degrees / one_rotation_turn_degrees;
+// 	double one_rotation_turn_degrees = 115; // customize to your robot
+// 	double rotations = degrees / one_rotation_turn_degrees;
 
-	drive(3, 200, 1);
-	elevator_mtr.move_relative(rotations, speed);
-	elevator1_mtr.move_relative(rotations, speed);
-	// elevator_mtr.move_velocity(600);
-	// elevator1_mtr.move_velocity(-600);
+// 	drive(3, 200, 1);
+// 	elevator_mtr.move_relative(rotations, speed);
+// 	elevator1_mtr.move_relative(rotations, speed);
+// 	// elevator_mtr.move_velocity(600);
+// 	// elevator1_mtr.move_velocity(-600);
 
-	rotations += elevator_mtr.get_position();
+// 	rotations += elevator_mtr.get_position();
 
-	while (!((elevator_mtr.get_position() < rotations + 0.5) &&
-			 (elevator1_mtr.get_position() > rotations - 0.5)))
-	{
-		pros::delay(20);
-	}
+// 	while (!((elevator_mtr.get_position() < rotations + 0.5) &&
+// 			 (elevator1_mtr.get_position() > rotations - 0.5)))
+// 	{
+// 		pros::delay(20);
+// 	}
 
-	pros::delay(wait);
-}
+// 	pros::delay(wait);
+// }
 
 // rotate the intakes
-void rotate(double degrees, int speed, int wait)
-{
+// void rotate(double degrees, int speed, int wait)
+// {
 
-	double one_rotation_turn_degrees = 115; // customize to your robot
-	double rotations = degrees / one_rotation_turn_degrees;
-	intake_mtr.move_relative(rotations, speed);
-	intake1_mtr.move_relative(rotations, speed);
+// 	double one_rotation_turn_degrees = 115; // customize to your robot
+// 	double rotations = degrees / one_rotation_turn_degrees;
+// 	intake_mtr.move_relative(rotations, speed);
+// 	intake1_mtr.move_relative(rotations, speed);
 
-	rotations += intake_mtr.get_position();
+// 	rotations += intake_mtr.get_position();
 
-	while (!((intake_mtr.get_position() < rotations + 0.5) &&
-			 (intake_mtr.get_position() > rotations - 0.5)))
-	{
-		pros::delay(20);
-	}
+// 	while (!((intake_mtr.get_position() < rotations + 0.5) &&
+// 			 (intake_mtr.get_position() > rotations - 0.5)))
+// 	{
+// 		pros::delay(20);
+// 	}
 
-	pros::delay(wait);
-}
+// 	pros::delay(wait);
+// }
