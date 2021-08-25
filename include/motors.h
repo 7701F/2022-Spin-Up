3cced17fd1d133a37fe6b2cf48f77dacbe5a2645
Chronomly <me@disilla.org>
2021-08-25 11:02:26 -0400

    Motor header file rework

--- include/motors.h
+++ include/motors.h
@@ -4,13 +4,13 @@
 // Declare motors
 extern pros::Motor leftMtr;
 extern pros::Motor rightMtr;
-// extern pros::Motor leftMtrR;
-// extern pros::Motor rightMtrR;
+extern pros::Motor leftMtrR;
+extern pros::Motor rightMtrR;
 // extern pros::Motor angler_mtr;
-extern pros::Motor intake_mtr;
-extern pros::Motor intake1_mtr;
-extern pros::Motor elevator_mtr;
-extern pros::Motor elevator1_mtr;
+// extern pros::Motor intake_mtr;
+// extern pros::Motor intake1_mtr;
+// extern pros::Motor elevator_mtr;
+// extern pros::Motor elevator1_mtr;
 // extern pros::Motor intakearm_mtr;
 // extern pros::Motor intakearm1_mtr;
 
