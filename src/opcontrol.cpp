d44eafde75e888d3ef7c633c93062762160dd511
Chronomly <me@disilla.org>
2021-08-31 19:57:23 -0400

    Add "parking" brake

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -6,6 +6,7 @@
 
 #include "main.h"
 
+bool pbrake = true;
 /**
  * Runs the operator control code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -29,17 +30,45 @@ void opcontrol()
 	while (true)
 	{
 
-		/* Arcade Steering
+		/* // Arcade Steering
 			int forward_backward = master.get_analog(ANALOG_LEFT_Y);
 			int left_right = master.get_analog(ANALOG_LEFT_X);
 			leftMtr.move(forward_backward + left_right);
 			rightMtr.move(forward_backward - left_right);
 		*/
+
 		// Move the steering motors
 		leftMtr.move(master.get_analog(ANALOG_LEFT_Y) + master.get_analog(ANALOG_LEFT_X));
 		rightMtr.move(master.get_analog(ANALOG_RIGHT_Y) + master.get_analog(ANALOG_RIGHT_Y));
 		leftMtrR.move(master.get_analog(ANALOG_LEFT_Y) + master.get_analog(ANALOG_LEFT_X));
 		rightMtrR.move(master.get_analog(ANALOG_RIGHT_Y) + master.get_analog(ANALOG_RIGHT_Y));
+
+		//
+		if (master.get_digital(DIGITAL_A) == 1)
+		{
+			pbrake = true;
+		}
+
+		if (master.get_digital(DIGITAL_B) == 1)
+		{
+			pbrake = false;
+		}
+
+		if (pbrake == true)
+		{
+			leftMtr.move(20);
+			rightMtr.move(20);
+			leftMtrR.move(-20);
+			rightMtrR.move(-20);
+		}
+
+		// if (pbrake == false)
+		// {
+		// 	leftMtr.move(master.get_analog(ANALOG_LEFT_Y) + master.get_analog(ANALOG_LEFT_X));
+		// 	rightMtr.move(master.get_analog(ANALOG_RIGHT_Y) + master.get_analog(ANALOG_RIGHT_Y));
+		// 	leftMtrR.move(master.get_analog(ANALOG_LEFT_Y) + master.get_analog(ANALOG_LEFT_X));
+		// 	rightMtrR.move(master.get_analog(ANALOG_RIGHT_Y) + master.get_analog(ANALOG_RIGHT_Y));
+		// }
 
 		// Lastly, delay
 		pros::delay(1);
