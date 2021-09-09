ef43f40bc91174463aba973ec683f4722d593e28
Michael Gummere <me@disilla.org>
2021-09-09 12:56:38 -0400

    Move brake code to opFunctions

--- include/opfunctions.h
+++ include/opfunctions.h
@@ -1,8 +1,13 @@
 #ifndef OPFUNC_H_
 #define OPFUNC_H_
 
+// Declare drive system variables.
+bool pbrake = false;
+
 // Declare driver functions
 void tankDrive();
 void arcadeDrive();
+void customBrake(bool pbrake);
+void prosBrake(bool pbrake);
 
 #endif
--- src/opFunctions.cpp
+++ src/opFunctions.cpp
@@ -25,3 +25,35 @@ void arcadeDrive()
 	leftMtrR.move(forward_backward + left_right);
 	rightMtrR.move(forward_backward - left_right);
 }
+
+void customBrake(bool pbrake)
+{
+	if (pbrake == true)
+	{
+		if (leftMtr.get_actual_velocity() != 0)
+		{
+			leftMtr.move(leftMtr.get_actual_velocity() * -1);
+			rightMtr.move(rightMtr.get_actual_velocity() * -1);
+			leftMtrR.move(leftMtrR.get_actual_velocity() * -1);
+			rightMtrR.move(rightMtrR.get_actual_velocity() * -1);
+		}
+	}
+}
+
+void prosBrake(bool pbrake)
+{
+	if (pbrake == true)
+	{
+		leftMtr.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+		rightMtr.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+		leftMtrR.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+		rightMtrR.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+	}
+	if (pbrake == false)
+	{
+		leftMtr.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+		rightMtr.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+		leftMtrR.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+		rightMtrR.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+	}
+}
