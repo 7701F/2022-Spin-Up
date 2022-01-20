d1c673c1864d87bc4506c517c07df94cb4027d51
Michael Gummere <me@disilla.org>
2022-01-20 13:54:34 -0500

    Display update WIP

--- src/display.cpp
+++ src/display.cpp
@@ -8,16 +8,19 @@
  */
 
 #include "main.h"
+#include <sstream>
+
+lv_obj_t *scr = lv_obj_create(NULL, NULL);
 
 // Magic beyond my understanding of LVGL
 void display()
 {
+	// Background Style
 	static lv_style_t background_style;
 	lv_style_copy(&background_style, &lv_style_plain);
 	background_style.body.main_color = LV_COLOR_BLACK;
 	background_style.body.grad_color = LV_COLOR_BLACK;
 
-	lv_obj_t *scr = lv_obj_create(NULL, NULL);
 	lv_obj_set_style(scr, &background_style);
 	lv_scr_load(scr);
 
@@ -37,23 +40,43 @@ void display()
 	lv_obj_set_style(display_title2, &title_style);
 	lv_label_set_text(display_title2, "SHALL WE PLAY A GAME?");
 	lv_obj_align(display_title2, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 35);
+}
 
+// IMU Status Shenanigans
+void imuDisplay()
+{
+	// styles
+	static lv_style_t title_style;
+	lv_style_copy(&title_style, &lv_style_plain);
+	title_style.text.font = &lv_font_dejavu_20;
+	title_style.text.color = LV_COLOR_GREEN;
 	lv_obj_t *display_title3 = lv_label_create(scr, NULL);
 	lv_obj_set_style(display_title3, &title_style);
 	lv_label_set_text(display_title3, "IMU STATUS: UNINITIALIZED");
 	lv_obj_align(display_title3, NULL, LV_ALIGN_IN_TOP_LEFT, 10, 85);
+	// should print about 2000 ms
 
 	int time = pros::millis();
 	int iter = 0;
 	while (imu_sensor.is_calibrating())
 	{
 		// printf();
-		lv_label_set_text(display_title3, "IMU calibrating... %d\n", iter);
+		std::ostringstream ss;
+		ss << "IMU calibrating... (" << iter << " ms)";
+		// std::string("IMU calibrating... ").append(txt).append(std::string(" ms)"))
+		lv_label_set_text(display_title3, ss.str().c_str());
 		iter += 10;
 		pros::delay(10);
 	}
-	// should print about 2000 ms
-	// lv_label_set_text(display_title3, "IMU is done calibrating (took %d ms)\n", iter - time);
+
+	if (imu_sensor.is_calibrating() == false)
+	{
+		std::ostringstream ss;
+		ss << "IMU calibrated: (" << iter - time << " ms)";
+		// std::string("IMU calibrated ").c_str() + txt.c_str() + std::string(" ms)").c_str()
+		// std::string txt = std::to_string(iter - time);
+		lv_label_set_text(display_title3, ss.str().c_str());
+	}
 }
 
 // Runs the display update code
