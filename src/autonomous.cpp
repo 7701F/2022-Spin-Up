5fabcac0ecf8b5eaa4a597e92bfc72760b3ea984
Michael Gummere <me@disilla.org>
2022-01-28 18:23:05 -0500

    move comment block

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -8,18 +8,6 @@
  */
 #include "main.h"
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-
 // Drive forward AND grab and pull tower auton
 /*
 void game_autonomous()
@@ -97,6 +85,18 @@ void trollingSkills() {
 	arms::chassis::move(5, 100);
 }
 
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
 // Auton Selector Logic
 void autonomous() {
 	switch (arms::selector::auton) {
