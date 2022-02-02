c69e43cee6822e16be67b429402adc8407b908d4
Michael Gummere <me@disilla.org>
2022-02-02 11:42:31 -0500

    Cleanup auton logic and remove non-working skills auton

--- include/ARMS/config.h
+++ include/ARMS/config.h
@@ -65,7 +65,7 @@ namespace arms::pid {
 // Auton selector configuration constants
 namespace arms::selector {
 // Names of autonomi, up to 10
-#define AUTONS "YLW Goal", "R WP", "L WP", "Test Auton", "Do Nothing"
+#define AUTONS "YLW Goal", "R WP", "L WP", "Do Nothing"
 #define HUE 360 // Color of theme from 0-359(H part of HSV)
 #define DEFAULT 3 // Default auton numbers
 } // namespace arms::selector
