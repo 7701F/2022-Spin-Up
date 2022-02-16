9f32803dd0bb1e929437e8eeeee92a0e662e215b
Michael Gummere <me@disilla.org>
2022-02-15 23:12:05 -0500

    begin auton 3.0 writing

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -15,41 +15,37 @@ std::int32_t mmToInch() {
 // Right win point
 void Rauton() {
 	arms::chassis::move(20, 80);
-	arms::claw::toggleClaw(false);
-	pros::delay(30);
+	arms::claw::toggleClaw();
 	liftMotors.moveRelative(30, 100);
 	pros::delay(300);
-	arms::chassis::turn(-60, 80);
+	arms::chassis::turnAsync(-60, 80);
 	arms::chassis::move(-20, 80);
-	arms::claw::toggleClaw(false);
+	arms::claw::toggleClaw();
 	pros::delay(30);
 }
 
 // Yellow goal
 void Yauton() {
-	arms::chassis::move(53, 80);
-	arms::chassis::moveAsync(11);
-	clawM.move_relative(210, 100);
-	arms::claw::waitUntilSettled();
+	arms::chassis::move(64, 80);
+	arms::claw::toggleClaw();
 	pros::delay(200);
 	arms::chassis::move(-50, 100);
+	arms::claw::toggleClaw();
 }
 
 // Left win point
 void Lauton() {
+	arms::claw::puncher();
+	arms::chassis::turn(90, 80);
 	arms::chassis::move(5, 50);
-	clawM.move_relative(170, 100);
-	pros::delay(300);
-	clawM.move_relative(40, 100);
+	arms::claw::toggleClaw();
 	arms::chassis::move(-15, 80);
 	pros::delay(500);
-	clawM.move_relative(-170, -50);
-	pros::delay(300);
 	arms::chassis::move(-6, 80);
 	arms::chassis::turn(-80, 60);
 	arms::chassis::move(60, 100);
 	arms::chassis::moveAsync(5, 75);
-	clawM.move_relative(210, 100);
+	arms::claw::toggleClaw();
 	pros::delay(300);
 	liftMotors.moveRelative(30, 75);
 	pros::delay(500);
@@ -66,11 +62,11 @@ void Sauton() {
 	arms::chassis::move(28, 60);
 	arms::chassis::turn(150, 50);
 	arms::chassis::move(80, 80);
-	clawM.move_relative(210, 50);
+	arms::claw::toggleClaw();
 	pros::delay(850);
 	arms::chassis::move(-80, 80);
 	arms::chassis::turn(45, 50);
-	clawM.move_relative(-210, 50);
+	arms::claw::toggleClaw();
 	pros::delay(300);
 	arms::chassis::turn(-85, 50);
 	arms::chassis::move(65, 80);
@@ -79,8 +75,6 @@ void Sauton() {
 
 // Programming Skills 2.0
 void Sauton2() {
-	// arms::chassis::resetAngle();
-	// std::int32_t dist = mmToInch();
 	// Fetch blue start MOGO
 	arms::chassis::move(8, 50);
 	// arms::chassis::turnAbsolute(180, 50);
@@ -96,7 +90,7 @@ void Sauton2() {
 	// Turn to face yellow right MOGO
 	arms::chassis::move(25, 100);
 	// Grab yellow right MOGO
-	arms::claw::toggleClaw(false);
+	arms::claw::toggleClaw();
 	arms::chassis::turn(90, 50);
 	arms::chassis::move(15, 80);
 	arms::chassis::turn(-90, 50);
@@ -105,7 +99,7 @@ void Sauton2() {
 	arms::chassis::turnAbsolute(0);
 	arms::chassis::move(10);
 	// Place yellow MOGO on balance board
-	arms::claw::toggleClaw(false);
+	arms::claw::toggleClaw();
 	// Move back to face yellow middle MOGO
 	arms::chassis::move(-10, 10);
 	arms::chassis::turnAsync(180, 50);
@@ -113,7 +107,7 @@ void Sauton2() {
 	arms::lift::waitUntilSettled();
 	arms::chassis::move(50, 50);
 	// Grab yellow middle MOGO
-	arms::claw::toggleClaw(false);
+	arms::claw::toggleClaw();
 	// Turn around to face the balance board
 	arms::chassis::move(4, 50);
 	arms::chassis::turnAbsolute(0, 50);
@@ -122,7 +116,7 @@ void Sauton2() {
 	arms::lift::waitUntilSettled();
 	arms::chassis::move(10, 50);
 	// Drop the mogo and move back to face the balance board
-	arms::claw::toggleClaw(false);
+	arms::claw::toggleClaw();
 	// Move back to face the yellow left MOGO
 	arms::chassis::move(-10, 50);
 	arms::chassis::turn(-180, 50);
@@ -135,7 +129,7 @@ void Sauton2() {
 	arms::chassis::move(30, 80);
 	arms::chassis::turnAbsolute(90, 50);
 	// Grab the yellow left MOGO
-	arms::claw::toggleClaw(false);
+	arms::claw::toggleClaw();
 	// Turn torwards the balance board
 	arms::chassis::turnAbsolute(90, 50);
 	// Drive to it
@@ -149,7 +143,7 @@ void Sauton2() {
 	arms::lift::waitUntilSettled();
 	arms::chassis::move(10, 50);
 	// Drop the MOGO
-	arms::claw::toggleClaw(false);
+	arms::claw::toggleClaw();
 	// Move back
 	arms::chassis::move(-30, 80);
 	liftMotors.moveAbsolute(3, 80);
@@ -161,15 +155,35 @@ void Sauton2() {
 	arms::chassis::turnAbsolute(-30, 50);
 	arms::chassis::move(50, 80);
 	// Grab it
-	arms::claw::toggleClaw(false);
+	arms::claw::toggleClaw();
 	arms::chassis::move(-10, 50);
 	arms::chassis::turnAbsolute(-15, 50);
 	// Move it to the red side
 	arms::chassis::move(50, 80);
 	// Drop it
-	arms::claw::toggleClaw(false);
+	arms::claw::toggleClaw();
 	// Move back to our side
 	arms::chassis::move(-70, 100);
+}
+
+// Programming Skills 3.0
+void Sauton3() {
+	arms::odom::init();
+	arms::odom::reset();
+	arms::chassis::resetAngle(180);
+	winchM.move_relative(-2065, -100);
+	pros::delay(3250);
+	arms::chassis::move(-78, 50);
+	winchM.move_relative(1100, 100);
+	pros::delay(1400);
+	arms::chassis::turn(90, 50);
+	ringM.move_velocity(600);
+	arms::chassis::move(50);
+	ringM.move_velocity(0);
+	arms::chassis::move(30);
+	arms::claw::toggleClaw();
+	arms::chassis::turn(-90);
+	arms::chassis::move(96);
 }
 
 /**
@@ -186,7 +200,6 @@ void Sauton2() {
 void autonomous() {
 	arms::chassis::resetAngle();
 	arms::pid::init();
-	// killTask();
 
 	// Auton Selector Logic
 	switch (arms::selector::auton) {
@@ -199,8 +212,7 @@ void autonomous() {
 		case -1:
 			arms::chassis::move(65, 80);
 			arms::chassis::moveAsync(10);
-			clawM.move_relative(210, 100);
-			arms::claw::waitUntilSettled();
+			arms::claw::toggleClaw();
 			pros::delay(200);
 			arms::chassis::move(-50, 100);
 			break;
