6ace1901e296cb90bc84632bde7990aeb094b40f
Michael Gummere <me@disilla.org>
2022-09-08 08:59:42 -0400

    update license notice

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,7 @@
 
   1. The origin of this software must not be misrepresented; you must not
      claim that you wrote the original software. If you use this software
-     in a product, an acknowledgment in the product documentation would be
-     appreciated and is required.
+     in a product, an acknowledgment in the product documentation is required. Your compliance is appreciated.
   2. Altered source versions must be plainly marked as such, and must not be
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
