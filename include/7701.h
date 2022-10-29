60c6bbb2f527d798b671f01a5702ef4e91088f9a
Michael Gummere <me@disilla.org>
2022-10-28 23:49:52 -0400

    Flywheel testing (#3)
    * update-arms
    
    * fix license notices
    
    * flywheel & conveyor fixes

--- include/7701.h
+++ include/7701.h
@@ -1,22 +1,22 @@
 /*
-	Copyright (c) 2019-2022 7701F
-	
-	This software is provided 'as-is', without any express or implied warranty. In no event will
-	the authors be held liable for any damages arising from the use of this software.
-	
-	Permission is granted to anyone to use this software for any purpose, including commercial
-	applications, and to alter it and redistribute it freely, subject to the following restrictions:
-	
-	1. The origin of this software must not be misrepresented; you must not claim that you wrote the
-	original software. If you use this software in a product, an acknowledgment (see the following)
-	in the product documentation is required.
-	
-	Portions Copyright (c) 2019-2022 7701F
-	
-	2. Altered source versions must be plainly marked as such, and must not be misrepresented as
-	being the original software.
-	
-	3. This notice may not be removed or altered from any source distribution.
+  Copyright (c) 2019-2022 7701F
+
+  This software is provided 'as-is', without any express or implied warranty. In no event will
+  the authors be held liable for any damages arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose, including commercial
+  applications, and to alter it and redistribute it freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not claim that you wrote the
+  original software. If you use this software in a product, an acknowledgment (see the following)
+  in the product documentation is required.
+
+  Portions Copyright (c) 2019-2022 7701F
+
+  2. Altered source versions must be plainly marked as such, and must not be misrepresented as
+  being the original software.
+
+  3. This notice may not be removed or altered from any source distribution.
 */
 
 #include "main.h"
@@ -50,6 +50,7 @@ extern okapi::MotorGroup leftMotors;
 
 /* Disk Launcher Motors */
 extern okapi::Motor flywheel;
+extern okapi::Motor conveyor;
 
 /* Disk Intake Motors */
 extern okapi::Motor intake;
