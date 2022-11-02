3db2ef39957a0830eaa6794aaf03ec0cb15142cc
Michael Gummere <me@disilla.org>
2022-11-02 10:59:41 -0400

    unify license header format

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,10 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
+#include "7701.h"
 #include "main.h"
 
 #include <sstream>
-
-#include "7701.h"
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
