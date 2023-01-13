07a5f5311515af0730641cda2f1c40256f5f809a
Michael Gummere <me@disilla.org>
2023-01-13 08:36:56 -0500

    tune PID

--- include/ARMS/config.h
+++ include/ARMS/config.h
@@ -35,11 +35,11 @@ namespace arms {
 #define SETTLE_THRESH_LINEAR .5 // amount of linear movement for settling
 #define SETTLE_THRESH_ANGULAR 1 // amount of angular movement for settling
 #define SETTLE_TIME 250         // amount of time to count as settled
-#define LINEAR_KP .45
-// #define LINEAR_KP 1
+// #define LINEAR_KP .45
+#define LINEAR_KP 1
 #define LINEAR_KI 0
-#define LINEAR_KD 5
-// #define LINEAR_KD 0
+// #define LINEAR_KD 5
+#define LINEAR_KD 0
 #define TRACKING_KP 60 // point tracking turning strength
 #define ANGULAR_KP 1
 #define ANGULAR_KI 0
